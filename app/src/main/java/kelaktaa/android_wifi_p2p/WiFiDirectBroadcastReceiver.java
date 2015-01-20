b18ebc5c6686eae574fc40fa7ac066c184a80f5b
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-01-21 00:37:03 +0100

    Constructor of the Broadcast receiver

--- app/src/main/java/kelaktaa/android_wifi_p2p/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/kelaktaa/android_wifi_p2p/WiFiDirectBroadcastReceiver.java
@@ -1,7 +1,22 @@
 package kelaktaa.android_wifi_p2p;
 
+import android.content.BroadcastReceiver;
+import android.net.wifi.p2p.WifiP2pManager;
+
+import java.nio.channels.Channel;
+
 /**
  * Created by Karim on 21/01/2015.
  */
-public class WiFiDirectBroadcastReceiver {
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
+    private WifiP2pManager mManager;
+    private Channel mChannel;
+    private MainActivity mActivity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel, MainActivity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+    }
 }
