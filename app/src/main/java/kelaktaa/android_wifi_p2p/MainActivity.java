8902b5fa612fb46973e75a9d0efdac6ac0010429
Karim El Aktaa <karim.elaktaa@gmail.com>
2015-01-21 02:01:29 +0100

    P2P Peers changed action

--- app/src/main/java/kelaktaa/android_wifi_p2p/MainActivity.java
+++ app/src/main/java/kelaktaa/android_wifi_p2p/MainActivity.java
@@ -1,19 +1,62 @@
 package kelaktaa.android_wifi_p2p;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    private WifiP2pManager mManager;
+    private Channel mChannel;
+    private BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+    final private static String DEBUG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.d(DEBUG_TAG, "Success DISCOVER");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                Log.d(DEBUG_TAG, "Fail DISCOVER");
+            }
+        });
     }
 
+    @Override
+         protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,12 +71,10 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/kelaktaa/android_wifi_p2p/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/kelaktaa/android_wifi_p2p/WiFiDirectBroadcastReceiver.java
@@ -1,9 +1,15 @@
 package kelaktaa.android_wifi_p2p;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
-
-import java.nio.channels.Channel;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.util.Log;
+import java.util.List;
 
 /**
  * Created by Karim on 21/01/2015.
@@ -12,11 +18,38 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
     private WifiP2pManager mManager;
     private Channel mChannel;
     private MainActivity mActivity;
+    final private static String DEBUG_TAG = "WiFiDirectBroadcastReceiver";
+    private PeerListListener myPeerListListener;
+    private WifiP2pDeviceList list;
 
+    public WiFiDirectBroadcastReceiver() {
+        super();
+    }
     public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel, MainActivity activity) {
         super();
         this.mManager = manager;
         this.mChannel = channel;
         this.mActivity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.d(DEBUG_TAG, "============ "+action);
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            Log.d(DEBUG_TAG, "WIFI_P2P_STATE_CHANGED_ACTION");
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            Log.d(DEBUG_TAG, "+++++++++++ WIFI_P2P_PEERS_CHANGED_ACTION");
+            if(mManager != null) {
+                mManager.requestPeers(mChannel, myPeerListListener);
+                myPeerListListener.onPeersAvailable(list);
+                //Log.d(DEBUG_TAG, "LISTE => "+ list.toString() + list.getDeviceList().size());
+            }
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            Log.d(DEBUG_TAG, "WIFI_P2P_CONNECTION_CHANGED_ACTION");
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            Log.d(DEBUG_TAG, "WIFI_P2P_THIS_DEVICE_CHANGED_ACTION");
+        }
     }
 }
